fd28996d712e833e5de43c832cde3f489fe89c42
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2025-10-14 14:46:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+const currentChamp = "philadelphia";
+
+let nextHome = "";
+let nextVisitor = "";
+let nextDate;
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return [
+    year,
+    month.toString().padStart(2, "0"),
+    day.toString().padStart(2, "0"),
+  ].join("-");
+}
+
+// schedule and upcoming game widget
+
+fetch("25-26-schedule.json")
+  .then((res) => res.json())
+  .then((res) => {
+    let schedule = {};
+
+    // reformatting the schedule downloaded from hockeyreference.com
+
+    res.forEach((game) => {
+      if (!Object.keys(schedule).includes(game.date)) {
+        schedule[game.date] = { games: [] };
+      }
+      schedule[game.date].games.push({
+        home: game.home,
+        visitor: game.visitor,
+      });
+    });
+
+    let found = 0;
+
+    for (let i = 0; i < 20; i++) {
+      let tempDate = new Date().setDate(new Date().getDate() + i);
+      tempDate = new Date(tempDate);
+      tempDate = formatDate(tempDate);
+
+      if (!schedule[tempDate]) {
+        continue;
+      }
+
+      schedule[tempDate].games.forEach((game) => {
+        if (Object.values(game).includes(currentChamp)) {
+          nextHome = game.home;
+          nextVisitor = game.visitor;
+          nextDate = tempDate;
+          found = 1;
+        }
+      });
+
+      if (found != 0) break;
+    }
+
+    // adding date and team logos for the next game
+
+    document.getElementById("date").innerText = new Date(
+      nextDate,
+    ).toLocaleString("en-us", {
+      timeZone: "UTC",
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+    });
+    document
+      .getElementById("home")
+      .setAttribute("src", `logos/${nextHome}.png`);
+    document
+      .getElementById("away")
+      .setAttribute("src", `logos/${nextVisitor}.png`);
+  });
+
+const teamPoints = [
+ { anaheim: "0" },
+  { boston: "0" },
+  { buffalo: "0" },
+  { calgary: "0" },
+  { carolina: "0" },
+  { chicago: "0" },
+  { colorado: "0" },
+  { columbus: "0" },
+  { dallas: "0" },
+  { detroit: "0" },
+  { edmonton: "0" },
+  { florida: "3" },
+  { vegas: "0" },
+  { "los-angeles": "0" },
+  { minnesota: "0" },
+  { montreal: "0" },
+  { nashville: "0" },
+  { "new-jersey": "0" },
+  { "ny-islanders": "0" },
+  { "ny-rangers": "0" },
+  { ottawa: "0" },
+  { philadelphia: "1" },
+  { pittsburgh: "0" },
+  { "san-jose": "0" },
+  { seattle: "0" },
+  { "st-louis": "0" },
+  { tampa: "0" },
+  { toronto: "0" },
+  { utah: "0" },
+  { vancouver: "0" },
+  { washington: "0" },
+  { winnipeg: "0" },
+];
+
+const Lagzdins = document.querySelector(".Lagzdins");
+const Zarins = document.querySelector(".Zarins");
+const Krists = document.querySelector(".Krists");
+const Davis = document.querySelector(".Davis");
+
+const players = [Lagzdins, Zarins, Krists, Davis];
+
+// iterates through team points array and displays all team points accordingly
+
+teamPoints.forEach((team) => {
+  const teamName = Object.keys(team)[0];
+  document.getElementById(teamName).innerText = team[teamName];
+});
+
+// adds a border to the active cup champ
+
+document.getElementById(currentChamp).parentElement.classList.add("champ");
+
+// gets all team points for each player and displays the max value of each next to the player name
+
+getPoints();
+
+function getPoints() {
+  players.forEach((player) => {
+    let playerTotal = 0;
+    const playerArray = [];
+    player.querySelectorAll(".team-score").forEach((team) => {
+      playerTotal += Number(team.innerText);
+      playerArray.push(Number(team.innerText));
+    });
+
+    player.querySelector("#high-points").innerText =
+      `High ${Math.max(...playerArray)} / Total ${playerTotal}`;
+  });
+}
+
+// Rules Modal functionality
+
+let rulesModal = document.getElementById("rules-modal");
+let modalOpen = document.getElementById("modal-open");
+let modalClose = document.getElementById("modal-close");
+
+modalOpen.addEventListener("click", () => {
+  rulesModal.showModal();
+});
+
+modalClose.addEventListener("click", () => {
+  rulesModal.close();
+});
+
+// const teamPoints23_24 = [
+//   { anaheim: "2" },
+//   { utah: "6" },
+//   { boston: "7" },
+//   { buffalo: "0" },
+//   { calgary: "5" },
+//   { carolina: "7" },
+//   { chicago: "1" },
+//   { colorado: "9" },
+//   { columbus: "1" },
+//   { dallas: "3" },
+//   { detroit: "0" },
+//   { edmonton: "0" },
+//   { florida: "3" },
+//   { vegas: "8" },
+//   { "los-angeles": "0" },
+//   { minnesota: "0" },
+//   { montreal: "2" },
+//   { nashville: "3" },
+//   { "new-jersey": "2" },
+//   { "ny-islanders": "0" },
+//   { "ny-rangers": "1" },
+//   { ottawa: "0" },
+//   { philadelphia: "2" },
+//   { pittsburgh: "4" },
+//   { "san-jose": "0" },
+//   { seattle: "2" },
+//   { "st-louis": "2" },
+//   { tampa: "0" },
+//   { toronto: "0" },
+//   { vancouver: "6" },
+//   { washington: "1" },
+//   { winnipeg: "2" },
+// ];
