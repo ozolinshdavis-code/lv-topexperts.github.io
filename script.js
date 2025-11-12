adb58d1bec1356735c929b321eb4f63aa53d5f07
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2025-11-12 08:51:29 +0200

    Change currentChamp to 'dallas' and update score

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "ottawa";
+const currentChamp = "dallas";
 
 let nextHome = "";
 let nextVisitor = "";
@@ -85,7 +85,7 @@ const teamPoints = [
   { chicago: "0" },
   { colorado: "0" },
   { columbus: "0" },
-  { dallas: "0" },
+  { dallas: "1" },
   { detroit: "0" },
   { edmonton: "0" },
   { florida: "3" },
