1e22903aa32f4c64fce0481a7c83d59cb1465303
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2025-12-28 10:32:47 +0200

    Update colorado value from 6 to 7

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const teamPoints = [
   { calgary: "0" },
   { carolina: "0" },
   { chicago: "0" },
-  { colorado: "6" },
+  { colorado: "7" },
   { columbus: "0" },
   { dallas: "3" },
   { detroit: "0" },
