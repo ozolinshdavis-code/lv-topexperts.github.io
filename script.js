a0dc79c7a9a553708136725cf4285ddf1a592163
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2026-03-11 09:08:29 +0200

    Update currentChamp and ny-islanders score

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "st-louis";
+const currentChamp = "ny-islanders";
 
 let nextHome = "";
 let nextVisitor = "";
@@ -95,7 +95,7 @@ const teamPoints = [
   { montreal: "2" },
   { nashville: "0" },
   { "new-jersey": "0" },
-  { "ny-islanders": "5" },
+  { "ny-islanders": "6" },
   { "ny-rangers": "3" },
   { ottawa: "2" },
   { philadelphia: "3" },
