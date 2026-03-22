ae821cca8a4df0d5bd1db088fa6ea4011125c331
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2026-03-22 21:41:24 +0200

    Change currentChamp from 'washington' to 'colorado'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentChamp = "washington";
+const currentChamp = "colorado";
 
 let nextHome = "";
 let nextVisitor = "";
@@ -83,7 +83,7 @@ const teamPoints = [
   { calgary: "0" },
   { carolina: "2" },
   { chicago: "0" },
-  { colorado: "10" },
+  { colorado: "11" },
   { columbus: "0" },
   { dallas: "3" },
   { detroit: "0" },
