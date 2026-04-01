b442671b924a36f340fb4f9143403027e24a70f7
ozolinshdavis-code <ozolinsh.davis@gmail.com>
2026-04-01 11:45:33 +0300

    Update winnipeg value from '4' to '5'

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const teamPoints = [
   { utah: "5" },
   { vancouver: "0" },
   { washington: "1" },
-  { winnipeg: "4" },
+  { winnipeg: "5" },
 ];
 
 const Lagzdins = document.querySelector(".Lagzdins");
